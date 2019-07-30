8cecab3d6b25714f0b04a88ed92d0eaca5e26dd8
dongxiaowen001@ke.com <dongxiaowen001@ke.com>
2019-07-30 15:29:10 +0800

    use argv

--- nodejs/filesystem/watcher.js
+++ nodejs/filesystem/watcher.js
@@ -1,4 +1,8 @@
 'use strict';
 const fs = require('fs');
-fs.watch('target.txt', () => console.log('File changed!'));
-console.log('Now watching target.txt for changes...');
+const filename = process.argv[2];
+if(!filename){
+    throw Error('A file to watch must be specified!')
+}
+fs.watch(filename, () => console.log(`File ${filename} changed!`));
+console.log(`Now watching ${filename} for changes...`);
