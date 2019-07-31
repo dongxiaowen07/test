08725a44603a6f8c8adfc09cd669b1524b8f7e97
dongxiaowen001@ke.com <dongxiaowen001@ke.com>
2019-07-31 15:02:10 +0800

    image filereadstream

--- nodejs/filesystem/helloworld-server.js
+++ nodejs/filesystem/helloworld-server.js
@@ -1,6 +1,7 @@
 var http = require('http');
+var fs = require('fs');
 http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
+    res.writeHead(200, {'Content-Type': 'image/jpeg'});
+    fs.createReadStream('./dog.jpg').pipe(res);
 }).listen(3000);
 console.log('Server running at http://localhost:3000/');
